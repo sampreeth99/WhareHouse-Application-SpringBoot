7c6669960c9515df7ed880396e629bdfc8d212f9
sampreeth <saisampreeth99@gmail.com>
2023-09-25 14:27:41 +0530

    Standard Project Structure Is Ready For Application Development

---
+++ WhareHouse_App/src/main/java/in/wh/WhareHouseAppApplication.java
@@ -0,0 +1,13 @@
+package in.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhareHouseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhareHouseAppApplication.class, args);
+	}
+
+}
---
+++ WhareHouse_App/src/test/java/in/wh/WhareHouseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.wh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhareHouseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
